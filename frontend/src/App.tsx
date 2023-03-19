607931139d87f1bef47b21af669f8b59e3ed916b
Henry Matsen <hdm32@byu.edu>
2023-03-19 16:54:55 -0600

    mission 12

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './MarchMadness.png';
 import './App.css';
+import data from './CollegeBasketballTeams.json';
+function Header() {
+  const logo = require('./MarchMadness.png');
+  return (
+    <div>
+      <header>
+        <img src={logo} className="titleImg" />
+      </header>
+      <p className="desc">List of every college team in March Madness</p>
+    </div>
+  );
+}
+class Team extends React.Component<any, any> {
+  render() {
+    const team = this.props;
+
+    return (
+      <div className="card">
+        <h2>{team.school}</h2>
+        <h3>Mascot: {team.name}</h3>
+        <h3>
+          Location: {team.city}, {team.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div className="cards">
+      {data.teams.map((team) => (
+        <Team {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <TeamList />
     </div>
   );
 }
